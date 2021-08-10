14c6fc93685d0d9aa8c540ac37084a66aceb8fbb
hebah <hebaalmusllam@gmail.com>
2021-08-11 01:20:32 +0300

    project finalized

---
+++ public/js/chat.js
@@ -0,0 +1,46 @@
+var socket = io();
+var messages = document.getElementById("messages");
+
+(function() {
+  $("form").submit(function(e) {
+    let li = document.createElement("li");
+    e.preventDefault(); 
+    socket.emit("chat message", $("#message").val());
+
+    messages.appendChild(li).append($("#message").val());
+    let span = document.createElement("span");
+    messages.appendChild(span).append("by " + "Sender" + ": " + "just now");
+
+    $("#message").val("");
+
+    return false;
+  });
+
+  socket.on("received", data => {
+    let li = document.createElement("li");
+    let span = document.createElement("span");
+    var messages = document.getElementById("messages");
+    messages.appendChild(li).append(data.message);
+    messages.appendChild(span).append("by " + "Sender" + ": " + "just now");
+    console.log("message sent");
+  });
+})();
+
+// fetching initial chat messages from the database
+(function() {
+  fetch("/chats")
+    .then(data => {
+      return data.json();
+    })
+    .then(json => {
+      json.map(data => {
+        let li = document.createElement("li");
+        let span = document.createElement("span");
+        messages.appendChild(li).append(data.message);
+        messages
+          .appendChild(span)
+          .append("by " + data.sender + ": " + formatTimeAgo(data.createdAt));
+      });
+    });
+})();
+
